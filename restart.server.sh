465edee93fe0393387c5484dcb50667013de8f23
enjoyzhou <369141517@qq.com>
2020-07-15 19:08:42 +0800

    v.001

---
+++ restart.server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+echo '证书签发成功，移动证书'
+cp -f /etc/v2ray/v2ray.crt /etc/v2ray/v2ray.key /home/
+service v2ray restart
---
+++ sign.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo '正在安装依赖'
+
+if cat /etc/os-release | grep "centos" > /dev/null
+    then
+    yum update > /dev/null
+    yum install tar wget -y > /dev/null
+else
+    apt update > /dev/null
+    apt-get install tar wget -y > /dev/null
+fi
+
+domain=$1
+cd ~
+wget https://github.com/enjoyZhou/acme_sign/raw/master/restart.server.sh
+wget -O -  https://get.acme.sh | sh
+alias acme.sh=~/.acme.sh/acme.sh
+
+acme.sh --install-cert -d $domain \
+--key-file       /etc/v2ray/v2ray.key  \
+--fullchain-file /etc/v2ray/v2ray.crt \
+--reloadcmd     ". ~/restart.server.sh"
